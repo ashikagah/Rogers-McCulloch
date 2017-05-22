afa371ce16ed818d38fb8e41a5a42d3d4c824918
Hiroshi Ashikaga <hashika1@jhmi.edu>
2017-05-21 22:07:32 -0400

    update

---
+++ demo.m
@@ -0,0 +1,12 @@
+%       ***************************************************
+%       *  Copyright (C) 2017, Hiroshi Ashikaga, MD, PhD  *
+%       *  hashika1@jhmi.edu                              *
+%       *  Cariac Arrhythmia Service                      *
+%       *  Johns Hopkins University School of Medicine    *
+%       *  Baltimore, Maryland, USA                       *
+%       *  5/21/2017                                      *
+%       ***************************************************
+
+%% Demo script for Rogers-McCulloch model
+
+% % % 
